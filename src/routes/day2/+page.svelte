4dd7d2c02968f1758b90cb9be13ca04a479cf722
aster <137767097+aster-void@users.noreply.github.com>
2025-01-24 01:09:18 +0900

    create day2 and reverse LSP order (+ fix lefthook to ignore unknown file types

---
+++ src/routes/day2/+page.svelte
@@ -0,0 +1,13 @@
+<script lang="ts">
+  const count = $state(0);
+</script>
+
+<center>
+  <div>
+    <span class="text-5xl">{count ?? "not set"}</span>
+  </div>
+
+  <div>
+    <input class="input input-bordered" type="number" bind:value={count} />
+  </div>
+</center>
