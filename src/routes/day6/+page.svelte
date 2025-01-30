6384fdd1de18f022a42077df4e0ff0b9e1170c82
aster <137767097+aster-void@users.noreply.github.com>
2025-01-30 13:32:15 +0900

    add another way to do client-only fetching

--- src/routes/day6/+page.svelte
+++ src/routes/day6/+page.svelte
@@ -1,24 +1,18 @@
 <script lang="ts">
-  // data streaming
-  const { data } = $props();
+  import { browser } from "$app/environment";
+
+  // another way for client-only fetching
+  const promise: Promise<unknown> = browser
+    ? fetch("https://jsonplaceholder.typicode.com/todos/1").then((res) =>
+        res.json(),
+      )
+    : new Promise(() => {});
 </script>
 
-<div>
-  <a class="btn btn-primary" href="/">Go Back</a>
-</div>
-
-{@render show(1, data.s1)}
-{@render show(2, data.s2)}
-{@render show(3, data.s3)}
-
-{#snippet show(count: number, data: Promise<unknown>)}
-  <p>
-    {#await data}
-      <span class="loading loading-spinner"></span>
-    {:then data}
-      {count}: {JSON.stringify(data)}
-    {:catch err}
-      <span class="text-error">something went wrong: {err.message}</span>
-    {/await}
-  </p>
-{/snippet}
+{#await promise}
+  <span class="loading loading-spinner"></span>
+{:then data}
+  {JSON.stringify(data)}
+{:catch err}
+  <span class="text-error">something went wrong: {err.message}</span>
+{/await}
---
+++ src/routes/day7/+page.svelte
@@ -0,0 +1,24 @@
+<script lang="ts">
+  // data streaming
+  const { data } = $props();
+</script>
+
+<div>
+  <a class="btn btn-primary" href="/">Go Back</a>
+</div>
+
+{@render show(1, data.s1)}
+{@render show(2, data.s2)}
+{@render show(3, data.s3)}
+
+{#snippet show(count: number, data: Promise<unknown>)}
+  <p>
+    {#await data}
+      <span class="loading loading-spinner"></span>
+    {:then data}
+      {count}: {JSON.stringify(data)}
+    {:catch err}
+      <span class="text-error">something went wrong: {err.message}</span>
+    {/await}
+  </p>
+{/snippet}
